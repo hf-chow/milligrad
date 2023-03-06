95afdb0dd9510d2bf6587cb231825030acc511ec
Hon Chow <nara@Hons-MacBook-Pro.local>
2023-03-07 01:02:37 +0800

    Build Node init

--- Node.py
+++ Node.py
@@ -1,6 +1,15 @@
-from Scaler import Scaler 
+from Scaler import Scaler
+import random
 
 class Node:
 
-    def __init__(self):
+    def __init__(self, numIn):
+        self.weights = [Scaler(random.uniform(-1, 1)) for i in range(numIn)]
+        self.biases = Scaler(random.uniform(-1, 1))
+
+    def foward(self):
         pass
+
+    def backprop(self):
+        pass
+
