2770223947e6f09e9ef6ccef393a0c57c9a300a6
Hon Chow <honfungchow@gmail.com>
2023-03-07 23:04:06 +0800

    Implement forward pass

--- Node.py
+++ Node.py
@@ -5,11 +5,22 @@ class Node:
 
     def __init__(self, numIn):
         self.weights = [Scaler(random.uniform(-1, 1)) for i in range(numIn)]
-        self.biases = Scaler(random.uniform(-1, 1))
+        self.bias = Scaler(random.uniform(-1, 1))
+        self.relu = Scaler
 
-    def foward(self):
-        pass
+    def forward(self, inputs):
+        output = Scaler(0)
+        zipped = zip(self.weights, inputs)
+        for weight, _input in zipped:
+            output += (weight*_input + self.bias).relu()
+        return output 
 
     def backprop(self):
         pass
 
+
+#inputs = [Scaler(3), Scaler(2), Scaler(-1)]
+#node = Node(len(inputs))
+
+#print(node.weights, node.bias)
+#print(node.forward(inputs))
