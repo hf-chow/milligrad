39b1c6937cac5d676d4824fee119757007d4affe
Hon Chow <honfungchow@gmail.com>
2023-03-11 16:35:29 +0800

    Implemented and tested forward method

--- Layer.py
+++ Layer.py
@@ -6,9 +6,18 @@ class Layer:
     def __init__(self, numIn, numOut):
         self.neurons = [Node(numIn) for i in range(numOut)]
 
+    def forward(self, inputs):
+        output = Scaler(0)
+        for n in self.neurons:
+            output += n.forward(inputs)
+        return output
+
 
 L = Layer(5,3)
 print(L.neurons)
 
+inputs = [Scaler(i) for i in range(5)]
+
 for i in L.neurons:
     print(i.weights, i.bias)
+    print(L.forward(inputs))
